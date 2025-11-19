28f7b4b2659ddda7a8aecb26b93c94003896f26c
Dawson-Codes <dawsontj@comcast.net>
2025-11-19 10:56:46 -0500

    buffer fixes

--- lib/core/buffer.py
+++ lib/core/buffer.py
@@ -10,11 +10,12 @@ class Buffer:
         self.buffer = [b''] * capacity
         self.write_index = 0
         self.count = 0
-        
-    def add_sample(self, temperatures=None, pressures=None, hk_voltages=None, hk_currents=None, hk_powers=None, hk_temps=None, hk_ina_temps=None, timestamp=None):
+
+    def add_sample(self, timestamp=None, temperatures=None, pressures=None, hk_voltages=None, hk_currents=None,
+                   hk_powers=None, hk_temps=None, hk_ina_temps=None):
         if timestamp is None:
             timestamp = time.ticks_ms() & 0xFFFFFFFF
-            
+
         temperatures = temperatures or [0.0, 0.0, 0.0, 0.0]
         pressures = pressures or [0.0, 0.0, 0.0, 0.0]
         hk_voltages = hk_voltages or [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -24,8 +25,9 @@ class Buffer:
         hk_ina_temps = hk_ina_temps or [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
         # Struct format: timestamp + (4 temps + 4 pressures) + (6V + 6I + 6P + 6T + 6INAT)
-        fmt = "<I6f12f"  # = 1 uint32 + 18 floats total
-        packed = struct.pack(fmt, timestamp, *temperatures, *pressures, *hk_voltages, *hk_currents, *hk_powers, *hk_temps, *hk_ina_temps)
+        fmt = "<I4f4f6f6f6f6f6f"  # = 1 uint32 + 38 floats total
+        packed = struct.pack(fmt, timestamp, *temperatures, *pressures, *hk_voltages, *hk_currents, *hk_powers,
+                             *hk_temps, *hk_ina_temps)
 
         self.buffer[self.write_index] = packed
         self.write_index = (self.write_index + 1) % self.capacity
@@ -33,7 +35,7 @@ class Buffer:
             self.count += 1
 
         return True
-    
+
     def get_all(self):
         if self.count == 0:
             return []
@@ -41,19 +43,19 @@ class Buffer:
             return self.buffer[:self.count]
         else:
             return self.buffer[self.write_index:] + self.buffer[:self.write_index]
-    
+
     def get_latest(self):
         if self.count == 0:
             return None
         index = (self.write_index - 1) % self.capacity
         return self.buffer[index]
-    
+
     def clear(self):
         self.write_index = 0
         self.count = 0
-    
+
     def is_full(self):
         return self.count >= self.capacity
-    
+
     def size(self):
         return self.count
