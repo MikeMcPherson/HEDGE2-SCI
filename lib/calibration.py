80fa0ef613ac6aef4b34ff877fcbfb65c10103ee
Dawson-Codes <dawsontj@comcast.net>
2025-12-02 18:29:24 -0500

    temperature and pressure offsets

---
+++ lib/calibration.py
@@ -0,0 +1,8 @@
+# HEDGE-2 SCI Board - Calibration Offsets
+# This file is auto-generated and updated by CLI calibration commands
+
+# Temperature offsets (4 thermocouples)
+TEMP_OFFSETS = [0.0, 0.0, 0.0, 0.0]
+
+# Pressure offsets (4 pressure sensors)
+PRESSURE_OFFSETS = [0.0, 0.0, 0.0, 0.0]
